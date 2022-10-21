6ef4d73135c3ec54baa9378c36b4f81371a41487
dylan nantz <nantz.dylan@gmail.com>
2022-10-21 02:50:06 +0000

    add some comments

--- HVZ.Models/User.cs
+++ HVZ.Models/User.cs
@@ -2,12 +2,30 @@
 namespace HVZ.Models;
 public class User
 {
+    /// <summary>
+    /// User's unique identification
+    /// </summary>
     public string Id { get; init; }
+    /// <summary>
+    /// Legal first name of the user
+    /// </summary>
     public string FirstName { get; init; }
+    /// <summary>
+    /// Legal last name of the user
+    /// </summary>
     public string LastName { get; init; }
+    /// <summary>
+    /// Email address of the user
+    /// </summary>
     public string Email { get; init; }
+    /// <summary>
+    /// Time this user was created
+    /// </summary>
     public Instant CreatedAt { get; init; }
 
+    /// <summary>
+    /// Class containing information about a specific user
+    /// </summary>
     public User(string id, string firstname, string lastname, string email)
     {
         Id = id;
