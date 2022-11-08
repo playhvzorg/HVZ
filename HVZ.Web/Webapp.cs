6da7a4417f8419789f0dd9a5f74044e0491b6163
Dylan Nantz <Nantz.Dylan@gmail.com>
2022-11-07 21:30:14 -0500

    create blazor app

--- HVZ.Web/Webapp.cs
+++ HVZ.Web/Webapp.cs
@@ -1,16 +1,49 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using HVZ.Web.Data;
 namespace HVZ.Web;
 public class Webapp
 {
     WebApplication app;
     public Webapp(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(
+            new WebApplicationOptions()
+            {
+                ApplicationName = "HVZ.Web"
+            }
+        );
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<WeatherForecastService>();
         app = builder.Build();
-        app.MapGet("/", () => "Hello World!");
     }
 
     public Task RunAsnyc()
     {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+
+        app.MapFallbackToPage("/_Host");
+
         return app.RunAsync();
     }
 }
+
+
+
