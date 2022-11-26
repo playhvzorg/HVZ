194474cc5c6c20126bf3964861e634d8c3261c45
Dylan Nantz <Nantz.Dylan@gmail.com>
2022-11-26 00:45:03 -0500

    add Simplify comparisons with IdEquatable abstract class

--- HVZ.Models/Game.cs
+++ HVZ.Models/Game.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using NodaTime;
 namespace HVZ.Models;
 
-public class Game
+public class Game : IdEquatable<Game>
 {
     /// <summary>
     /// Unique name of this game.
@@ -93,14 +93,5 @@ public class Game
         return $"HVZ.Game@{Name}.{Id}";
     }
 
-    public override bool Equals(object? obj)
-    {
-        //TODO: this can likely be improved
-        return obj != null && this.ToString().Equals(obj.ToString());
-    }
-
-    public override int GetHashCode()
-    {
-        return ToString().GetHashCode();
-    }
+    protected override object EqualityId => ToString();
 }
---
+++ HVZ.Models/IdEquatable.cs
@@ -0,0 +1,21 @@
+namespace HVZ.Models;
+
+public abstract class IdEquatable<T> : IEquatable<T> where T : IdEquatable<T>
+{
+    protected abstract object EqualityId { get; }
+
+        public bool Equals(T? other)
+            => !ReferenceEquals(other, null) && EqualityId.Equals(other.EqualityId);
+
+        public static bool operator ==(IdEquatable<T>? a, IdEquatable<T>? b)
+            => ReferenceEquals(a, b) || !ReferenceEquals(a, null) && a.EqualityId.Equals(b?.EqualityId);
+
+        public static bool operator !=(IdEquatable<T>? a, IdEquatable<T>? b)
+            => !(a == b);
+
+        public override bool Equals(object? obj)
+            => obj as T == this;
+
+        public override int GetHashCode()
+            => EqualityId.GetHashCode();
+}
