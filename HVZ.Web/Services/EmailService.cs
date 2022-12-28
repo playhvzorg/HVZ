bfe2fc4f59596d726b54f2d50938855671be96e8
hrice9 <hrice9@uncc.edu>
2022-12-28 11:30:00 -0500

    email template html files

--- HVZ.Web/Services/EmailService.cs
+++ HVZ.Web/Services/EmailService.cs
@@ -26,79 +26,20 @@ namespace HVZ.Web.Services
             client.Credentials = new NetworkCredential(opts.EmailId, opts.Password);
             address = new MailAddress(opts.EmailId);
             client.EnableSsl = true;
+
+            string s = String.Format(System.IO.File.ReadAllText("Services/Templates/Sample.html"), "World");
+            System.Console.WriteLine(s);
         }
 
-        public void SendDebug(string email)
-        {
-            try
-            {
-                MailMessage msg = new MailMessage();
-                msg.Subject = "Sent from my web app!";
-                msg.Body = "Hello from my web app!";
-                msg.To.Add(email);
-                msg.From = address;
-
-                client.Send(msg);
-                msg.Dispose();
-            }
-            catch (SmtpException e)
-            {
-                // TODO: use logger instead
-                System.Console.WriteLine(e);
-            }
-        }
-
-        public void SendTemplateDebug(string email, string name)
-        {
-            string htmlBody = String.Format(@"
-            
-            <div style=""font-family: Arial"">
-                <h1>Hello, {0}</h1>
-            </div>
-            
-            ", name);
-
-            MailMessage msg = new MailMessage();
-            msg.Subject = "Sent from my web app!";
-            msg.Body = htmlBody;
-            msg.To.Add(new MailAddress(email));
-            msg.From = address;
-            msg.IsBodyHtml = true;
-
-            client.Send(msg);
-            msg.Dispose();
-        }
+        
 
         public async Task SendVerificationEmailAsync(string to, string name, string requestId)
         {
-            string htmlBody = String.Format(@"
-            
-            <div style=""font-family: Arial"">
-                <h1>Confirm your email</h1>
-            </div>
-            <hr>
-            <p style=""font: 14pt Arial;"">Hello {0}</p>
-            <p style=""font: 14pt Arial;"">Thank you for signing up at playhvz.org!</p>
-            <p style=""font: 14pt Arial;"">Please confirm your identity by verifying your email address</p>
-
-            <a
-            style=""
-            font: 14pt Arial;
-            text-decoration: none;
-            background-color: rgb(83, 124, 216);
-            color: white;
-            padding: 10px 14px 10px 14px;
-            border-radius: .25rem;
-            ""
-            href=""{2}Account/Verify?requestId={1}""
-            target=""_blank"">Verify email</a>
-            <p style=""margin-bottom: 1px; font: 14pt Arial;"">Or copy and paste this URL into your browser:</p>
-            <p style=""font: 14pt Arial;"">{2}Account/Verify?requestId={1}</p>
-            ", name, requestId, domainName);
+            string htmlBody = await ReadEmailTemplateAsync("VerificationEmailTemplate.html");
 
             MailMessage msg = new MailMessage();
             msg.Subject = "PlayHVZ: Confirm email address";
-            msg.Body = htmlBody;
+            msg.Body = String.Format(htmlBody, name, requestId, domainName);
             msg.To.Add(new MailAddress(to));
             msg.From = address;
             msg.IsBodyHtml = true;
@@ -108,40 +49,21 @@ namespace HVZ.Web.Services
 
         public async Task SendPasswordChangeEmailAsync(string to, string name, string requestId)
         {
-            string htmlBody = String.Format(@"
-
-            <div style=""font-family: Arial"">
-                <h1>Reset your password</h1>
-            </div>
-
-            <hr>
-            <p style=""font: 14pt Arial;"">Hello {0}</p>
-            <p style=""font: 14pt Arial;"">Click this button to reset your PlayHVZ account password</p>
-            <a
-            style=""
-            font: 14pt Arial;
-            text-decoration: none;
-            background-color: rgb(83, 124, 216);
-            color: white;
-            padding: 10px 14px 10px 14px;
-            border-radius: .25rem;
-            ""
-            href=""{2}Account/Reset?requestId={1}""
-            target=""_blank"">Reset password</a>
-            <p style=""margin-bottom: 1px; font: 14pt Arial;"">Or copy and paste this URL into your browser:</p>
-            <p style=""font: 14pt Arial;"">{2}Account/Reset?requestId={1}</p>
-            <hr>
-            <p style=""font: 14pt Arial;"">If you did not request a password reset, you can safely delete this email.</p>
-            ", name, requestId, domainName);
+            string htmlBody = await ReadEmailTemplateAsync("PasswordResetEmailTemplate.html");
 
             MailMessage msg = new MailMessage();
             msg.Subject = "PlayHVZ: Verify email address";
-            msg.Body = htmlBody;
+            msg.Body = String.Format(htmlBody, name, requestId, domainName);
             msg.To.Add(new MailAddress(to));
             msg.From = address;
             msg.IsBodyHtml = true;
 
             client.SendAsync(msg, requestId);
+        }
+
+        private async Task<string> ReadEmailTemplateAsync(string templateName)
+        {
+            return await File.ReadAllTextAsync($"Services/Templates/{templateName}");
         }
     }
 }
