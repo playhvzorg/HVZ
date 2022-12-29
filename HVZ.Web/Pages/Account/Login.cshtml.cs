7be38225c18f79d33bfebd144b5fa533975da29f
hrice9 <hrice9@uncc.edu>
2022-12-29 01:41:26 -0500

    log IP addresses for users on login warn on multiple signin failures

--- HVZ.Web/Pages/Account/Login.cshtml.cs
+++ HVZ.Web/Pages/Account/Login.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using HVZ.Web.Identity.Models;
 
 namespace HVZ.Web.Pages
@@ -13,23 +14,19 @@ namespace HVZ.Web.Pages
         private SignInManager<ApplicationUser> signInManager;
         private NavigationManager navigation;
         private ILogger<LoginModel> logger;
-        private string redirectUrl = "";
+        private HttpContextAccessor httpContextAccessor;
 
         [BindProperty]
         public SignInUserModel UserModel { get; set; }
 
-        public LoginModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, NavigationManager navigation, ILogger<LoginModel> logger)
+        public LoginModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, NavigationManager navigation, ILogger<LoginModel> logger, HttpContextAccessor httpContextAccessor)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.UserModel = new();
             this.navigation = navigation;
             this.logger = logger;
-        }
-
-        public void OnGet(string returnUrl = "/")
-        {
-            this.redirectUrl = returnUrl;
+            this.httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = "/")
@@ -45,15 +42,42 @@ namespace HVZ.Web.Pages
             {
                 // Sign in and redirect to home
                 ApplicationUser? authUser = await userManager.FindByEmailAsync(UserModel.Email);
+                string? ip = httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString();
                 if (authUser != null)
                 {
+                    if (ip == null)
+                    {
+                        // Figure out what went wrong
+                        if(httpContextAccessor.HttpContext == null)
+                        {
+                            logger.LogError($"HTTP Context is null\nUser Database ID: {authUser.DatabaseId}\nUser Identity ID: {authUser.Id}\nUser Email: {authUser.Email}");
+                        }
+                        else if (httpContextAccessor.HttpContext.Connection.RemoteIpAddress == null)
+                        {
+                            logger.LogError($"Remote connection is null\nUser Database ID: {authUser.DatabaseId}\nUser Identity ID: {authUser.Id}\nUser Email: {authUser.Email}");
+                        }
+                    }
+
                     Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(authUser, UserModel.Password, UserModel.RememberMe, false);
                     if (result.Succeeded)
                     {
                         logger.LogDebug("Successful login");
+                        logger.LogInformation($"User: {{\n\tName: {authUser.FullName}\n\tEmail: {authUser.Email}\n\tDatabase ID: {authUser.DatabaseId}\n\tIdentity ID: {authUser.Id} \n}}\nLogin from: {ip ?? "Could not determine IP address"}");
+                        await userManager.ResetAccessFailedCountAsync(authUser);
                         return Redirect(returnUrl);
                     }
+                    else
+                    {
+                        // Valid emai, invalid password
+                        await userManager.AccessFailedAsync(authUser);
+                        int numFailedAttempts = await userManager.GetAccessFailedCountAsync(authUser);
+                        if (numFailedAttempts > 1)
+                        {
+                            logger.LogWarning($"{numFailedAttempts} consecutive failed login attempts for email {authUser.Email}\nAttempt made from IP: {ip ?? "Could not determine IP address"}");
+                        }
+                    }
                 }
+
                 logger.LogDebug("Unsuccessful login attempt");
                 ModelState.AddModelError(nameof(UserModel.Email), "Login Failed: Invalid Email or Password");
             }
