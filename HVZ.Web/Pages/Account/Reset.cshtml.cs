a5cc2bae87b52d1786f21f7afee6cdd8796fa47b
Howstheaqua <hughes.e.47201@gmail.com>
2022-12-31 16:03:35 -0500

    add password reset

---
+++ HVZ.Web/Pages/Account/Reset.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Components;
+using HVZ.Web.Identity.Models;
+using HVZ.Web.Services;
+using System.Web;
+namespace HVZ.Web.Pages
+{
+    public class ResetPageModel:PageModel
+    {
+        private UserManager<ApplicationUser> userManager;
+        private IHttpContextAccessor httpContextAccessor;
+        private ILogger<LoginModel> logger;
+        private SignInManager<ApplicationUser> signInManager;
+        [BindProperty]
+        public ResetModel ResetModelProperty { get; set; }
+        public ResetPageModel(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor, ILogger<LoginModel> logger, EmailService emailService, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.httpContextAccessor = httpContextAccessor;
+            this.logger = logger;
+            this.signInManager = signInManager;
+            this.ResetModelProperty = new();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string requestId, string userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (ResetModelProperty == null)
+            {
+                return Page();
+            }
+            ApplicationUser? appUser = await userManager.FindByIdAsync(userId);
+            if (appUser == null)
+            {   
+                logger.LogDebug($"Could not find user with id: {userId}");
+                return Page();
+            }
+            var result = await userManager.ResetPasswordAsync(appUser, requestId, ResetModelProperty.Password);
+            if( result.Succeeded)
+            {
+                await signInManager.PasswordSignInAsync(appUser, ResetModelProperty.Password, false, false);
+                return Redirect("/");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    logger.LogError($"{error.Code}:{error.Description}");
+                }
+            }
+            return Page();
+        }
+    }
+}
---
+++ HVZ.Web/Pages/Account/ResetRequest.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Components;
+using HVZ.Web.Identity.Models;
+using HVZ.Web.Services;
+using System.Web;
+namespace HVZ.Web.Pages
+{
+    public class ResetRequestPageModel:PageModel
+    {
+        private UserManager<ApplicationUser> userManager;
+        private IHttpContextAccessor httpContextAccessor;
+        private ILogger<LoginModel> logger;
+        private EmailService emailService;
+        [BindProperty]
+        public ResetRequestModel ResetRequestModelProperty { get; set; }
+        public ResetRequestPageModel(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor, ILogger<LoginModel> logger, EmailService emailService)
+        {
+            this.userManager = userManager;
+            this.httpContextAccessor = httpContextAccessor;
+            this.logger = logger;
+            this.emailService = emailService;
+            this.ResetRequestModelProperty = new();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (ResetRequestModelProperty == null)
+            {
+                return Page();
+            }
+            ApplicationUser? appUser = await userManager.FindByEmailAsync(ResetRequestModelProperty.Email);
+            if (appUser == null)
+            {
+                return Page();
+            }
+            string passwordResetToken = await userManager.GeneratePasswordResetTokenAsync(appUser);
+            await emailService.SendPasswordChangeEmailAsync(appUser.Email, appUser.FullName, HttpUtility.UrlEncode(passwordResetToken), appUser.Id.ToString());
+            return Page();
+        }
+    }
+}
