f52f89ed6a63cb5996ca744649417f7f5be40f99
Dylan Nantz <Nantz.Dylan@gmail.com>
2023-02-06 15:46:29 -0500

    add database event log

--- HVZ.Persistence/IGameRepo.cs
+++ HVZ.Persistence/IGameRepo.cs
@@ -50,12 +50,13 @@ public interface IGameRepo
     /// <summary>
     /// Sets isActive for a game
     /// </summary>
-    public Task<Game> SetActive(string gameName, bool active);
+    public Task<Game> SetActive(string gameName, bool active, string instigatorId);
 
     /// <summary>
     /// Set the <see cref="HVZ.Models.Player.gameRole"/> of a player
     /// </summary>
-    public Task<Game> SetPlayerToRole(string gameName, string userId, Player.gameRole role);
+    /// <param name="instigatorId">User who is causing the player to change role</param>
+    public Task<Game> SetPlayerToRole(string gameName, string userId, Player.gameRole role, string instigatorId);
 
     /// <summary>
     /// Log a tag in the specified game
@@ -77,23 +78,32 @@ public interface IGameRepo
     public Task<List<Game>> GetActiveGamesWithUser(string userId, int? limit = null);
 
     /// <summary>
+    /// Get the event log
+    /// </summary>
+    public Task<List<GameEventLog>> GetGameEventLog(string gameId);
+
+    /// <summary>
     /// Event that fires when a new game is created
     /// </summary>
     public event EventHandler<GameUpdatedEventArgs> GameCreated;
     /// <summary>
     /// Event that fires when a player joins a game
     /// </summary>
-    public event EventHandler<GameUpdatedEventArgs> PlayerJoinedGame;
+    public event EventHandler<PlayerUpdatedEventArgs> PlayerJoinedGame;
     /// <summary>
     /// Event that fires when a player's role is changed for a game
     /// </summary>
-    public event EventHandler<GameUpdatedEventArgs> PlayerRoleChanged;
+    public event EventHandler<PlayerRoleChangedEventArgs> PlayerRoleChanged;
     /// <summary>
-    /// Event that first when a tag is logged in a game
+    /// Event that fires when a tag is logged in a game
     /// </summary>
-    public event EventHandler<GameUpdatedEventArgs> TagLogged;
+    public event EventHandler<TagEventArgs> TagLogged;
     /// <summary>
-    /// Event that fires when a game is updated, such as changing isActive status
+    /// Event that fires when a game's isActive status is changed
+    /// </summary>
+    public event EventHandler<GameActiveStatusChangedEventArgs> GameActiveStatusChanged;
+    /// <summary>
+    /// Event that fires when a game is updated in a way not covered by other events
     /// </summary>
     public event EventHandler<GameUpdatedEventArgs> GameUpdated;
 
@@ -102,8 +112,62 @@ public interface IGameRepo
 public class GameUpdatedEventArgs : EventArgs
 {
     public Game game { get; init; }
-    public GameUpdatedEventArgs(Game g)
+    public string updatorId { get; init; }
+    public GameUpdatedEventArgs(Game g, string id)
     {
         game = g;
+        updatorId = id;
+    }
+}
+
+public class PlayerUpdatedEventArgs : EventArgs
+{
+    public Game game { get; init; }
+    public Player player { get; init; }
+    public PlayerUpdatedEventArgs(Game g, Player p)
+    {
+        game = g;
+        player = p;
+    }
+}
+
+public class PlayerRoleChangedEventArgs : EventArgs
+{
+    public Game game { get; init; }
+    public Player player { get; init; }
+    public string instigatorId { get; init; }
+    public Player.gameRole Role { get; init; }
+    public PlayerRoleChangedEventArgs(Game g, Player p, string instigatorid, Player.gameRole role)
+    {
+        game = g;
+        player = p;
+        instigatorId = instigatorid;
+        Role = role;
+    }
+}
+
+public class TagEventArgs : EventArgs
+{
+    public Game game { get; init; }
+    public Player Tagger { get; init; }
+    public Player TagReciever { get; init; }
+    public TagEventArgs(Game g, Player tagger, Player tagreciever)
+    {
+        game = g;
+        Tagger = tagger;
+        TagReciever = tagreciever;
+    }
+}
+
+public class GameActiveStatusChangedEventArgs : EventArgs
+{
+    public Game game { get; init; }
+    public string updatorId { get; init; }
+    public bool Active { get; init; }
+    public GameActiveStatusChangedEventArgs(Game g, string id, bool active)
+    {
+        game = g;
+        updatorId = id;
+        Active = active;
     }
 }
