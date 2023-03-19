b28b5db5d096e3be7f225b9035982918331c26ce
Dylan Nantz <Nantz.Dylan@gmail.com>
2023-03-19 12:33:02 -0400

    Add redundant mongodb hosts

--- HVZ.Web/Settings/MongoConfig.cs
+++ HVZ.Web/Settings/MongoConfig.cs
@@ -7,10 +7,18 @@ namespace HVZ.Web.Settings
         public string? UserName { get; set; }
         public string? Password { get; set; }
         public string? DatabaseName { get; set; }
-        public string? Host { get; set; }
-        public int Port { get; set; }
-        public string ConnectionString => UserName is not null ?
-            $"mongodb://{UserName}:{Password}@{Host}:{Port}" :
-            $"mongodb://{Host}:{Port}";
+        public string? Host1 { get; set; }
+        public int Port1 { get; set; }
+        public string? Host2 { get; set; }
+        public int Port2 { get; set; }
+        public string? Host3 { get; set; }
+        public int Port3 { get; set; }
+
+        public string ConnectionString =>
+            "mongodb://" +
+            (UserName is not null ? $"{UserName}:{Password}@" : "") +
+            $"{Host1}:{Port1}" +
+            (Host2 is not null ? $"{Host2}:{Port2}" : "") + 
+            (Host3 is not null ? $"{Host3}:{Port3}" : "");
     }
 }
