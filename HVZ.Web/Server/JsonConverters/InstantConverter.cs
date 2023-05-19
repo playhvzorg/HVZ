891a1558b3da44aa1aa4ceb335fd8e63e52285e2
hrice9 <hrice9@uncc.edu>
2023-05-18 23:24:58 -0400

    formatting

--- HVZ.Web/Server/JsonConverters/InstantConverter.cs
+++ HVZ.Web/Server/JsonConverters/InstantConverter.cs
@@ -5,14 +5,19 @@ namespace HVZ.Web.Server.JsonConverters
 {
     public class InstantConverter : JsonConverter<Instant>
     {
-        public override Instant ReadJson(JsonReader reader, Type objectType, Instant existingValue, bool hasExistingValue, JsonSerializer serializer)
-        {
-            return Instant.FromDateTimeUtc(reader.ReadAsDateTime()!.Value);
-        }
+        public override Instant ReadJson(
+            JsonReader reader,
+            Type objectType,
+            Instant existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer
+        ) => Instant.FromDateTimeUtc(reader.ReadAsDateTime()!.Value);
 
-        public override void WriteJson(JsonWriter writer, Instant value, JsonSerializer serializer)
-        {
-            writer.WriteValue(new DateTime(value.ToUnixTimeMilliseconds()));
-        }
+
+        public override void WriteJson(
+            JsonWriter writer,
+            Instant value,
+            JsonSerializer serializer
+        ) => writer.WriteValue(new DateTime(value.ToUnixTimeTicks()));
     }
 }
